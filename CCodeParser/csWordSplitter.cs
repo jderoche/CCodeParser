765d844e1f8b0fd1d2f6a55e857434e3f64bfeeb
Joel Deroche <jederaliz@gmail.com>
2018-03-05 18:36:11 +0100

    First Commit

---
+++ CCodeParser/csWordSplitter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCodeParser
+{
+    /// <summary>
+    /// Class used to splitter a text in list of words
+    /// > List<string> SplitText(string text)
+    /// > AddSeparator(string strSeparator)
+    /// </summary>
+    public class csWordSplitter
+    {
+        #region Private
+
+        /// <summary>
+        /// List of separator used to split the word
+        /// </summary>
+        List<string> separatorList;
+
+        /// <summary>
+        /// List of word after split process
+        /// </summary>
+        List<string> wordList;
+
+        #endregion
+
+        #region Public
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="textToSplit"></param>
+        /// <returns></returns>
+        public List<string> SplitText(string textToSplit)
+        {
+            if (textToSplit != "")
+            {
+                wordList = new List<string>();
+            }
+
+            return (wordList);
+        }
+
+        /// <summary>
+        /// Update the separator list
+        /// </summary>
+        /// <param name="separator"></param>
+        public void AddSeparator(string separator)
+        {
+            separatorList.Add(separator);
+        }
+
+        #endregion
+    }
+}
