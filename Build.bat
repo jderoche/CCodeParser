d7575fc5721e64e7e327fabaaffc20828bc6de39
Joel Deroche <jederaliz@gmail.com>
2018-03-06 15:53:51 +0100

    Update for auto build and test

--- Build.bat
+++ Build.bat
@@ -1,1 +1,6 @@
 MSBuild.exe CCodeParser.sln
+pause
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\mstest.exe"  /testcontainer:"UnitTest\bin\Debug\UnitTest.dll"
+pause
+cd TestResults
+type *.trx
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CCodeParser;
+
+namespace UnitTestCCodeParser
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSplitTextWithNullText()
+        {
+
+            csWordSplitter wordSplitterClass = new csWordSplitter();
+            Assert.IsNull(wordSplitterClass.SplitText(""));
+
+        }
+
+        [TestMethod]
+        public void TestSplitTextWithNotNullText()
+        {
+
+            csWordSplitter wordSplitterClass = new csWordSplitter();
+            Assert.IsNotNull(wordSplitterClass.SplitText(" "));
+
+        }
+    }
+}
